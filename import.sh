f672267167c30558f933fec40c7213a8511dfa13
isolachine <keanemu@gmail.com>
2018-03-22 08:22:39 -0400

    Initial Commit

---
+++ import.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_dir=$(pwd)/$(dirname $0)
+
+files=`ls $current_dir/data/*.txt`
+for filename in $files
+do
+	datetime="`date +%Y/%m/%d\ %H:%M:%S`"
+    echo "$datetime Processing file: $filename"
+    influx -import -path=$filename -precision=h -database=air
+done
---
+++ realtime.py
@@ -0,0 +1,28 @@
+from influxdb import InfluxDBClient
+import threading
+import random
+
+interval = 5
+
+
+def insert_data():
+    json_body = [
+        {
+            "measurement": "realtime",
+            "fields": {
+                "data_value": random.uniform(0, 10.0)
+            }
+        }
+    ]
+    client.write_points(json_body)
+
+def start_timer():
+    threading.Timer(interval, start_timer).start()
+    insert_data()
+
+
+client = InfluxDBClient('localhost', 8086, 'root', 'root', 'realtime')
+client.create_database('realtime')
+client.query('CREATE RETENTION POLICY "one_hour" ON "realtime" DURATION 1h REPLICATION 1')
+client.query('CREATE CONTINUOUS QUERY "cq_one_min" ON "realtime" BEGIN SELECT mean(data_value) INTO realtime.autogen.realtime_mean FROM realtime.one_hour.realtime GROUP BY time(1m) END')
+start_timer()
